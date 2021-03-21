65f55041c545e849a169cb1cc07f73f63d0b201c
DOCOPAC123 <rohitchira@gmail.com>
2021-03-21 10:49:28 +0530

    Added the Theory Folder & Coding File

---
+++ src/Array/FindNumberInArray.java
@@ -0,0 +1,11 @@
+package Array;
+import java.util.*;
+
+public class FindNumberInArray {
+
+    public static void main(String [] args) {
+        System.out.println("Enter the value of n:\n");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+    }
+}
